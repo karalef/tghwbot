6a21e6fee627c178fa66054e05171ce76edb48bd
Toffee-iZt <mgsiask@gmail.com>
2022-07-21 23:16:48 +0300

    restructure help command

--- bot/builtin.go
+++ bot/builtin.go
@@ -5,36 +5,26 @@ import (
 	"tghwbot/bot/tg"
 )
 
-var ping = Command{
-	Cmd:         "ping",
-	Description: "check the bot for availability",
-	Run: func(ctx *Context, _ *tg.Message, _ []string) {
-		ctx.Send("pong")
-	},
-}
-
-var help = Command{
-	Cmd:         "help",
-	Description: "help",
-	Args: []Arg{
-		{
-			Name: "command",
+func MakeHelp(b *Bot) *Command {
+	h := Command{
+		Cmd:         "help",
+		Description: "help",
+		Args: []Arg{
+			{
+				Name: "command",
+			},
 		},
-	},
-}
-
-func makeHelp(b *Bot) *Command {
-	h := help
+	}
 	h.Run = func(ctx *Context, msg *tg.Message, args []string) {
 		if len(args) > 0 {
 			for _, c := range b.cmds {
 				if c.Cmd != args[0] {
 					continue
 				}
-				h, e := generateHelp(c)
-				ctx.ReplyClose(h, &SendOptions{Entities: e})
+				h, e := c.generateHelp()
+				ctx.Reply(h, e...)
 			}
-			ctx.ReplyClose("command not found")
+			ctx.Reply("command not found")
 		}
 		var sb strings.Builder
 		sb.WriteString("Commands list\n")
@@ -44,39 +34,7 @@ func makeHelp(b *Bot) *Command {
 			sb.WriteByte(Prefix)
 			sb.WriteString(c.Cmd + " - " + c.Description)
 		}
-		ctx.ReplyClose(sb.String())
+		ctx.Reply(sb.String())
 	}
 	return &h
-}
-
-func generateHelp(c *Command) (string, []tg.MessageEntity) {
-	sb := strings.Builder{}
-	sb.WriteByte(Prefix)
-	sb.WriteString(c.Cmd)
-	for _, a := range c.Args {
-		sb.WriteByte(' ')
-		if a.Required {
-			sb.WriteByte('[')
-		} else {
-			sb.WriteByte('{')
-		}
-		sb.WriteString(a.Name)
-
-		if len(a.Consts) > 0 {
-			sb.WriteByte(':')
-			sb.WriteString("\"" + strings.Join(a.Consts, "\"|\"") + "\"")
-		}
-		if a.Required {
-			sb.WriteByte(']')
-		} else {
-			sb.WriteByte('}')
-		}
-	}
-	sb.WriteByte('\n')
-	sb.WriteString(c.Description)
-	return sb.String(), []tg.MessageEntity{tg.MessageEntity{
-		Type:   "pre",
-		Offset: 0,
-		Length: sb.Len() - len(c.Description) - 1,
-	}}
 }
--- bot/command.go
+++ bot/command.go
@@ -1,6 +1,9 @@
 package bot
 
-import "tghwbot/bot/tg"
+import (
+	"strings"
+	"tghwbot/bot/tg"
+)
 
 // Prefix is the character with which commands must begin.
 const Prefix = '/'
@@ -20,4 +23,36 @@ type Arg struct {
 	Required bool
 	Name     string
 	Consts   []string
+}
+
+func (c *Command) generateHelp() (string, []tg.MessageEntity) {
+	sb := strings.Builder{}
+	sb.WriteByte(Prefix)
+	sb.WriteString(c.Cmd)
+	for _, a := range c.Args {
+		sb.WriteByte(' ')
+		if a.Required {
+			sb.WriteByte('[')
+		} else {
+			sb.WriteByte('{')
+		}
+		sb.WriteString(a.Name)
+
+		if len(a.Consts) > 0 {
+			sb.WriteByte(':')
+			sb.WriteString("\"" + strings.Join(a.Consts, "\"|\"") + "\"")
+		}
+		if a.Required {
+			sb.WriteByte(']')
+		} else {
+			sb.WriteByte('}')
+		}
+	}
+	sb.WriteByte('\n')
+	sb.WriteString(c.Description)
+	return sb.String(), []tg.MessageEntity{{
+		Type:   "pre",
+		Offset: 0,
+		Length: sb.Len() - len(c.Description) - 1,
+	}}
 }
