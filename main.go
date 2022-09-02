79a2c3f798a2343e9a816cd72129d4edd3749f3d
Toffee-iZt <mgsiask@gmail.com>
2022-09-02 22:45:06 +0300

    customize time zone

--- main.go
+++ main.go
@@ -12,47 +12,56 @@ import (
 	"tghwbot/modules/images"
 	"tghwbot/modules/random"
 	"tghwbot/modules/text"
+	"time"
 )
 
 var color = flag.Bool("color", false, "use colored log")
 
 func init() {
+	loc, err := time.LoadLocation(os.Getenv("LOG_TIME_ZONE"))
+	if err != nil {
+		panic(err)
+	}
+	time.Local = loc
 	flag.Parse()
 }
 
 func main() {
-	log := logger.New(logger.NewWriter(os.Stderr, *color), "HwBot")
+	log := logger.Default("HwBot", *color)
 	log.Info("PID: %d", os.Getpid())
 
-	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
-
-	b, err := bot.NewWithContext(ctx, bot.Config{
-		Token:    os.Getenv("TOKEN"),
+	b, err := bot.New(os.Getenv("TOKEN"), bot.Config{
 		Logger:   log,
 		MakeHelp: true,
 		Commands: []*bot.Command{
 			&debug.DebugCmd,
 			&random.Info,
 			&random.Number,
+			&random.Roll,
 			&random.When,
 			&text.Gen,
 			&images.CitgenCmd,
 			&images.Search,
 		},
+		Handler: bot.Handler{
+			OnInlineQuery: images.OnInline,
+		},
 	})
 	if err != nil {
 		panic(err)
 	}
-	b.OnInlineQuery = images.OnInline
 
-	err = b.Run(0)
-
-	switch err {
-	case nil:
-		log.Info("stopping without error")
-	case context.Canceled:
-		log.Info("stopping by os signal")
-	default:
+	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	err = b.RunContext(ctx)
+	if err != nil {
 		log.Error("bot finished with an error: %s", err)
+		return
+	}
+
+	select {
+	case <-ctx.Done():
+		log.Info("stopped by os signal")
+	default:
+		log.Info("stopping without error")
 	}
 }
